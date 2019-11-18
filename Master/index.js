bb2e92f301acc7df2b06419a25e6ab27d4efba61
David Watson <davidjosephwatson@gmail.com>
2019-11-18 18:15:02 -0500

    finalized for second submission

--- Master/index.js
+++ Master/index.js
@@ -37,150 +37,150 @@ const QUIZ = {
       ],
       correctAnswer: 'Tacking'
     },
-    // {//3
-    //   text: 'What is the name of the line which is used to raise or lower the sails?',
-    //   answers: [
-    //     {
-    //       text: 'Sheet',
-    //     },
-    //     {
-    //       text: 'Teather',
-    //     },
-    //     {
-    //       text: 'Painter',
-    //     },
-    //     {
-    //       text: 'Halyard'
-    //     }  
-    //   ],
-    //   correctAnswer: 'Halyard'
-    // },
-    // {//4
-    //   text: 'When a sailboat is pointing directly into the wind and all forward momentum has ceased, the boat is said to be what?',
-    //   answers: [
-    //     {
-    //       text: 'Close hauled',      
-    //     },
-    //     {
-    //       text: 'Pointing',
-    //     },
-    //     {
-    //       text: 'In irons'
-    //     },
-    //     {
-    //       text: 'Stalled',
-    //     }
-    //   ],
-    //   correctAnswer: 'In irons'
-    // },
-    // {//5
-    //   text: 'When a sailboat "turtles", it is what?',
-    //   answers: [
-    //     {
-    //       text: 'Taking water over the bow',
-    //     },
-    //     {
-    //       text: 'Has dramatically slowed down',
-    //     },
-    //     {
-    //       text: 'Taking water of the stern',
-    //     },
-    //     {
-    //       text: 'Completely upside down',
-    //     } 
-    //   ],
-    //   correctAnswer: 'Completely upside down'
-    // },
-    // {//6
-    //   text: 'Which of the following is not a type of sailboat?',
-    //   answers: [
-    //     {
-    //       text: 'Yawl',
-    //     },
-    //     {
-    //       text: 'Tabernacle'
-    //     },
-    //     {
-    //       text: 'Schooner',
-    //     },
-    //     {
-    //       text: 'Dinghy',
-    //     } 
-    //   ],
-    //   correctAnswer: 'Tabernacle'
-    // },
-    // {//7
-    //   text: 'A sailing vessel which is under sail usually has the right of way over all of the following except what?',
-    //   answers: [
-    //     {
-    //       text: 'A container ship'
-    //     },
-    //     {
-    //       text: 'Another boat approaching from the left',
-    //     },
-    //     {
-    //       text: 'A similar sized vessel which is being powered by a motor',
-    //     },
-    //     {
-    //       text: 'A jet ski',
-    //     } 
-    //   ],
-    //   correctAnswer: 'A container ship'
-    // },
-    // {//8
-    //   text: 'You are returning to port after a day of sailing. Ahead of you is a channel marked by colored buoys. How should you enter the channel?',
-    //   answers: [
-    //     {
-    //       text: 'With all of the buoys on your left',
-    //     },
-    //     {
-    //       text: 'To the left of the black and white buoy and to the left of the green buoy',
-    //     },
-    //     {
-    //       text: 'To the right of the black and white buoy and to the left of the red buoy',
-    //     },
-    //     {
-    //       text: 'Anywhere you want as long as you stay between the buoys',
-    //     } 
-    //   ],
-    //   correctAnswer: 'To the right of the black and white buoy and to the left of the red buoy'
-    // },
-    // {//9
-    //   text: 'The lines which are used to control a sails direction in relation to the direction of the wind are called what?',
-    //   answers: [
-    //     {
-    //       text: 'Sheets'
-    //     },
-    //     {
-    //       text: 'Halyards',
-    //     },
-    //     {
-    //       text: 'Ropes',
-    //     },
-    //     {
-    //       text: 'Outhaul',
-    //     } 
-    //   ],
-    //   correctAnswer: 'Sheets'
-    // },
-    // {//10
-    //   text: 'Most sails have three corners. Which of the following is not the name of one of the corners?',
-    //   answers: [
-    //     {
-    //       text: 'Head',
-    //     },
-    //     {
-    //       text: 'Luff',
-    //     },
-    //     {
-    //       text: 'Tack',
-    //     },
-    //     {
-    //       text: 'Clew',
-    //     } 
-    //   ],
-    //   correctAnswer: 'Luff'
-    // }
+    {//3
+      text: 'What is the name of the line which is used to raise or lower the sails?',
+      answers: [
+        {
+          text: 'Sheet',
+        },
+        {
+          text: 'Teather',
+        },
+        {
+          text: 'Painter',
+        },
+        {
+          text: 'Halyard'
+        }  
+      ],
+      correctAnswer: 'Halyard'
+    },
+    {//4
+      text: 'When a sailboat is pointing directly into the wind and all forward momentum has ceased, the boat is said to be what?',
+      answers: [
+        {
+          text: 'Close hauled',      
+        },
+        {
+          text: 'Pointing',
+        },
+        {
+          text: 'In irons'
+        },
+        {
+          text: 'Stalled',
+        }
+      ],
+      correctAnswer: 'In irons'
+    },
+    {//5
+      text: 'When a sailboat "turtles", it is what?',
+      answers: [
+        {
+          text: 'Taking water over the bow',
+        },
+        {
+          text: 'Has dramatically slowed down',
+        },
+        {
+          text: 'Taking water of the stern',
+        },
+        {
+          text: 'Completely upside down',
+        } 
+      ],
+      correctAnswer: 'Completely upside down'
+    },
+    {//6
+      text: 'Which of the following is not a type of sailboat?',
+      answers: [
+        {
+          text: 'Yawl',
+        },
+        {
+          text: 'Tabernacle'
+        },
+        {
+          text: 'Schooner',
+        },
+        {
+          text: 'Dinghy',
+        } 
+      ],
+      correctAnswer: 'Tabernacle'
+    },
+    {//7
+      text: 'A sailing vessel which is under sail usually has the right of way over all of the following except what?',
+      answers: [
+        {
+          text: 'A container ship'
+        },
+        {
+          text: 'Another boat approaching from the left',
+        },
+        {
+          text: 'A similar sized vessel which is being powered by a motor',
+        },
+        {
+          text: 'A jet ski',
+        } 
+      ],
+      correctAnswer: 'A container ship'
+    },
+    {//8
+      text: 'You are returning to port after a day of sailing. Ahead of you is a channel marked by colored buoys. How should you enter the channel?',
+      answers: [
+        {
+          text: 'With all of the buoys on your left',
+        },
+        {
+          text: 'To the left of the black and white buoy and to the left of the green buoy',
+        },
+        {
+          text: 'To the right of the black and white buoy and to the left of the red buoy',
+        },
+        {
+          text: 'Anywhere you want as long as you stay between the buoys',
+        } 
+      ],
+      correctAnswer: 'To the right of the black and white buoy and to the left of the red buoy'
+    },
+    {//9
+      text: 'The lines which are used to control a sails direction in relation to the direction of the wind are called what?',
+      answers: [
+        {
+          text: 'Sheets'
+        },
+        {
+          text: 'Halyards',
+        },
+        {
+          text: 'Ropes',
+        },
+        {
+          text: 'Outhaul',
+        } 
+      ],
+      correctAnswer: 'Sheets'
+    },
+    {//10
+      text: 'Most sails have three corners. Which of the following is not the name of one of the corners?',
+      answers: [
+        {
+          text: 'Head',
+        },
+        {
+          text: 'Luff',
+        },
+        {
+          text: 'Tack',
+        },
+        {
+          text: 'Clew',
+        } 
+      ],
+      correctAnswer: 'Luff'
+    }
   ],
   currentQuestion: 0,
   score: 0
